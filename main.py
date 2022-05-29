d2f9a76f43273574d5ffcbaed377de30ddec9d0a
Miguel <miguelmazuelo92@gmail.com>
2022-05-29 19:15:32 +0200

    Update

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-import threading
+#!/bin/python3
+
+#import threading
+import multiprocessing as mp
 import queue
 import time
 import random
 import argparse
 
-class Productor(threading.Thread):
-    def __init__(self, queue, lock,):
+class Productor(mp.Process):
+    def __init__(self, queue, lock):
         self.queue = queue
         self.lock = lock
     
@@ -13,47 +16,51 @@ class Productor(threading.Thread):
         while True:
             self.lock.acquire()
             try:
+                if (self.queue.full()):
+                    self.lock.wait()
                 self.queue.put(random.random())
                 time.sleep(0.01)
             finally:
                 self.lock.release()
                 return self.queue
 
-class Consumidor(threading.Thread):
+class Consumidor(mp.Process):
     def __init__(self, queue, lock, funct):
         self.queue = queue
         self.lock = lock
         self.funct = funct
+
     def run(self):
         while True:
+            self.lock.acquire()
             try:
+                if (self.queue.empty()):
+                    self.lock.wait()
                 item = self.queue.get()
-                return self.funct(item)
+                print(item)
             finally:
-                self.wait()
+                self.lock.release()
+                return self.funct(item)
 
 def printed(name):
     print(str(name))
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("threads", type=int)
-    n_processes = parser.parse_args().threads
+    parser.add_argument("processes", type=int)
+    n_processes = parser.parse_args().processes
     
     q = queue.Queue(maxsize=5)
-    lock = threading.Lock()
+    lock = mp.Lock()
     processes_prod = []
     processes_cons = []
 
     for _ in range(n_processes):
-        p = threading.Thread(target=Productor, args=(q, lock,))
+        p = mp.Process(name="Producer", target=Productor, args=(q, lock,))
+        c = mp.Process(name="Consumer", target=Consumidor, args=(q, lock, printed,))
         processes_prod.append(p)
-
-    for _ in range(n_processes):
-        c = threading.Thread(target=Consumidor, args=(q, lock, printed,))
         processes_cons.append(c)
 
-    
     for p, c in zip(processes_prod, processes_cons):
         p.start()
         c.start()
